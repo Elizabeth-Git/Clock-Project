fbb3ff0bd0dac0a83443ab1fd3f2c8f4f3dbaa85
Elizabeth-Git <55302419+Elizabeth-Git@users.noreply.github.com>
2020-01-24 08:32:42 -1000

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,19 @@
 	var currentTime = new Date();
 	var hours = currentTime.getHours();
 	var minutes = currentTime.getMinutes();
-  var seconds = currentTime.getSeconds();
-  
-  var clockDiv = document.getElementById('clock');
-  
-  clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+ 	var seconds = currentTime.getSeconds();
+  	var clockDiv = document.getElementById('clock');
+  clockDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem;
+	 setInterval(displayTime, 1000);
+if (seconds < 10) {
+    seconds = "0" + seconds;
+	 var meridiem = "AM";
+}
+if (hours > 12) {
+    hours = hours - 12;
+    meridiem = "PM";
+ }
+if (hours === 0) {
+    hours = 12;    
+}
 }

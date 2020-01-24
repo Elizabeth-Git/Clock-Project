18f94a54d2707330a7a53691fe3e9292f1eda997
Elizabeth-Git <55302419+Elizabeth-Git@users.noreply.github.com>
2020-01-24 08:43:28 -1000

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
- function displayTime() {
-	var currentTime = new Date();
-	var hours = currentTime.getHours();
-	var minutes = currentTime.getMinutes();
- 	var seconds = currentTime.getSeconds();
-  	var clockDiv = document.getElementById('clock');
-  clockDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem;
-	 setInterval(displayTime, 1000);
-if (seconds < 10) {
-    seconds = "0" + seconds;
-	 var meridiem = "AM";
-}
-if (hours > 12) {
-    hours = hours - 12;
-    meridiem = "PM";
- }
-if (hours === 0) {
-    hours = 12;    
-}
-}
+function displayTime() {
+        var currentTime = new Date();
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+        var meridiem = "AM"; 
+        if (hours > 12) {
+            hours = hours - 12;
+            meridiem = "PM";
+        }
+        if (hours === 0) {
+            hours = 12;    
+        }
+        if(hours < 10) {
+            hours = "0" + hours;
+        }
+        if(minutes < 10) {
+            minutes = "0" + minutes;
+        }        
+        if(seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        var clockDiv = document.getElementById('clock');
+        clockDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem;
+    }
+    displayTime();
+    setInterval(displayTime, 1000);
+
+});

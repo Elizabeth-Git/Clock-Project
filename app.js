0bca74473c3f034ee51dbb59418f4bd5d1803f5a
Elizabeth-Git <55302419+Elizabeth-Git@users.noreply.github.com>
2020-01-24 08:19:35 -1000

    Update app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+ function displayTime() {
+	var currentTime = new Date();
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  
+  var clockDiv = document.getElementById('clock');
+  
+  clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+}
